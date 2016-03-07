cd607f1c3c2b199b6668816084c94f969bb2d493
Bruno Sampaio <bens.sampaio@gmail.com>
2016-03-07 17:33:31 +0100

    Improve mirror styles

--- src/jquery.autogrow.js
+++ src/jquery.autogrow.js
@@ -4,77 +4,82 @@
  * Licensed under the MIT license
  */
 (function($) {
-	var pluginName = 'autogrow';
+    var pluginName = 'autogrow';
 
-	var textareaClass = pluginName+'-field';
-	var mirrorClass = pluginName+'-mirror';
+    var textareaClass = pluginName+'-field';
+    var mirrorClass = pluginName+'-mirror';
 
-	$.fn[pluginName] = function(options)
-	{
-		return this.each(function(index, element) {
-			if(element.tagName != 'TEXTAREA') return true;
+    $.fn[pluginName] = function(options)
+    {
+        return this.each(function(index, element) {
+            if(element.tagName != 'TEXTAREA') return true;
 
-			var textarea = $(element);
-			var mirror = $('<div></div>');
+            var textarea = $(element);
+            var mirror = $('<div></div>');
 
-			// Add mirror classes
-			mirror.addClass(mirrorClass);	
+            // Add mirror classes
+            mirror.addClass(mirrorClass);
 
-			// Add options properties to mirror element
-			if(options !== undefined) {
-				if(options.id !== undefined) mirror.attr('id', index == 0? options.id : options.id+'-'+index); 
-				if(options.classes !== undefined) mirror.addClass(options.classes);
-			}
+            // Add options properties to mirror element
+            if(options !== undefined) {
+                if(options.id !== undefined) mirror.attr('id', index == 0? options.id : options.id+'-'+index);
+                if(options.classes !== undefined) mirror.addClass(options.classes);
+            }
 
-			textarea
+            textarea
 
-				// Add textarea classes
-				.addClass(textareaClass)
+            // Add textarea classes
+                .addClass(textareaClass)
 
-				// On input change textarea height
-				.on('input keyup', function(event) {
-					var message = textarea.val();
+                // On input change textarea height
+                .on('input keyup', function(event) {
+                    var message = textarea.val();
 
-					// Add a non breakable space to the message if it is empty or the last character is a line break (otherwise line break height is ignored)
-					if(!message || message.charAt(message.length - 1) == '\n') message+= '\xA0';
+                    // Add a non breakable space to the message if it is empty or the last character is a line break (otherwise line break height is ignored)
+                    if(!message || message.charAt(message.length - 1) == '\n') message+= '\xA0';
 
-					// Parse message content
-					// Replace all line breaks in the message by the line break HTML element and add message to mirror element
-					message = message
-						.replace(/&/g, '&amp;')
-						.replace(/"/g, '&quot;')
-						.replace(/'/g, '&#39;')
-						.replace(/</g, '&lt;')
-						.replace(/>/g, '&gt;')
-						.replace(/ /g, '&nbsp;')
-						.replace(/\n/g, '<br/>');
+                    // Parse message content
+                    // Replace all line breaks in the message by the line break HTML element and add message to mirror element
+                    message = message
+                        .replace(/&/g, '&amp;')
+                        .replace(/"/g, '&quot;')
+                        .replace(/'/g, '&#39;')
+                        .replace(/</g, '&lt;')
+                        .replace(/>/g, '&gt;')
+                        .replace(/ /g, '&nbsp;')
+                        .replace(/\n/g, '<br/>');
 
-					// Add text to message field
-					mirror.html(message);
+                    // Add text to message field
+                    mirror.html(message);
 
-					// Set textarea height equal to mirror height
-					textarea.height(mirror.height());
-				})
-			
-				// On resize change mirror styles
-				.off(pluginName+'.resize')
-				.on(pluginName+'.resize', function() {
-					mirror.css({
-						'border' : textarea.css('border'),
-						'font-family' : textarea.css('font-family'),
-						'font-size' : textarea.css('font-size'),
-						'font-weight' : textarea.css('font-weight'),
-						'letter-spacing' : textarea.css('letter-spacing'),
-						'line-height' : textarea.css('line-height'),
-						'padding' : textarea.css('padding'),
-						'width' : textarea.css('width'),
-						'zoom' : textarea.css('zoom')
-					});
-				})
-				.trigger(pluginName+'.resize')
+                    // Set textarea height equal to mirror height
+                    textarea.height(mirror.height());
+                })
 
-				// Add mirror to DOM
-				.after(mirror);
-		});
-	}
+                // On resize change mirror styles
+                .off(pluginName+'.resize')
+                .on(pluginName+'.resize', function() {
+                    mirror.css({
+                        'border': textarea.css('border'),
+                        'font-family': textarea.css('font-family'),
+                        'font-size': textarea.css('font-size'),
+                        'font-weight': textarea.css('font-weight'),
+                        'letter-spacing': textarea.css('letter-spacing'),
+                        'line-height': textarea.css('line-height'),
+                        'padding': textarea.css('padding'),
+                        'position': 'absolute',
+                        'visibility': 'hidden',
+                        'white-space': 'normal',
+                        'width': textarea.css('width'),
+                        'word-break': 'break-work',
+                        'z-index': -1,
+                        'zoom': textarea.css('zoom'),
+                    });
+                })
+                .trigger(pluginName+'.resize')
+
+                // Add mirror to DOM
+                .after(mirror);
+        });
+    }
 })(jQuery);
